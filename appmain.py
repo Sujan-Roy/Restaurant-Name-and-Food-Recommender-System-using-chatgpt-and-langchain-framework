534b0d89dd4f5438ff578353b9d08fad9518c807
Sujan Chandra Roy <sujan.007.ice@gmail.com>
2023-10-26 14:24:18 +0900

    Update main.py

--- appmain.py
+++ appmain.py
@@ -2,16 +2,19 @@ import streamlit as st
 import langchainhelper as lch
 st.title("Restaurant Name and Food Recommender System")
 
-name=st.sidebar.selectbox("Select a country",["Bangladesh","India","Japan","USA","Russia", "Thailand"])
+
+
+#name=st.sidebar.selectbox("Select a country",["Bangladesh","India","Japan","USA","Russia", "Thailand"])
+user_input = st.text_input("Enter a country name")
 
 
 
-if name:
-    response= lch.generate_restaurant_name_and_foodItems(name)
-
+if user_input:
+    response= lch.generate_restaurant_name_and_foodItems(user_input)
+    st.write("Restaurant Name according to",user_input)
     st.header(response['restaurant_name'].strip())
-    food_items= response['food_item'].strip().split(",")
     st.write("**Food Items**")
+    food_items= response['food_item'].strip().split(",")
     for food_item in food_items:
         st.write(food_item)
  
